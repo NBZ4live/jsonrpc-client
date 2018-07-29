aecaa159ca256f738546f39f148b8a5c40e51524
Sergey Fayngold <sergey@faynhost.com>
2018-07-29 18:19:21 +0200

    Start a new hard fork

---
+++ readme.md
@@ -0,0 +1,2 @@
+# JSON-RPC Client (Laravel 5, Lumen 5)
+This repository is a hard fork of tochka-developers/jsonrpc-client.
---
+++ src/ServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tochka\JsonRpcClient;
+
+use Illuminate\Support\ServiceProvider as BaseServiceProvider;
+
+class ServiceProvider extends BaseServiceProvider
+{
+    public function boot()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                ClientGenerator::class
+            ]);
+        }
+
+        $this->publishes([
+            __DIR__ . '/../config/jsonrpcclient.php' => base_path('config/jsonrpcclient.php'),
+        ], 'config');
+        
+    }
+
+    public function register()
+    {
+        // TODO: Implement register() method.
+    }
+}
